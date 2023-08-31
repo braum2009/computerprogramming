1c9be6abf80d0e5d5b4016e90a1b4a0acfc74a46
Scholar <scholar@paradigm.local>
2023-08-31 13:33:11 -0600

    pdm

---
+++ Intro to Program Development Methodology.py
@@ -0,0 +1,16 @@
+MAIN_CHARACTER = "Mario
+main_character = "Luigi
+myNameIsNot = 'Braum'
+my_name = 'Bill
+MyName = 'Bob
+persom = "Barrett
+for 1 in range(18):
+    print(1)
+list = {'one, two, three,'}
+dictionary = {
+'name' : 'Barrett',
+'age': 16
+
+}
+def is_even_number(number):
+if number %2 == 
