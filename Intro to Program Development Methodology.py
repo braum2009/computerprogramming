7cf3f31e7515430db08b124bd2be9c1cbc6c3862
Scholar <scholar@paradigm.local>
2023-09-05 13:27:47 -0600

    variables and a function

--- Intro to Program Development Methodology.py
+++ Intro to Program Development Methodology.py
@@ -1,9 +1,9 @@
-MAIN_CHARACTER = "Mario
-main_character = "Luigi
+MAIN_CHARACTER = "Mario"
+main_character = "Luigi"
 myNameIsNot = 'Braum'
-my_name = 'Bill
-MyName = 'Bob
-persom = "Barrett
+my_name = 'Bill'
+MyName = 'Bob'
+persom = "Barrett"
 for 1 in range(18):
     print(1)
 list = {'one, two, three,'}
@@ -13,4 +13,4 @@ dictionary = {
 
 }
 def is_even_number(number):
-if number %2 == 
+If number %2 == 
---
+++ braum methol assignment.py
@@ -0,0 +1,11 @@
+my_name = "Braum"
+test_score= 80 
+was_lunch_tasty = True
+Classes = ["spanish, Math, computer promgramming, art meditations "]
+
+def add_two_numbers(num1, num2):
+    print(num1 + num2)
+add_two_numbers(89, 46)
+user_number1 = input("give me a 2nd number:")
+
+
