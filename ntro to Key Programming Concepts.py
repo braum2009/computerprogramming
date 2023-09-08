c3372f517f4647bb6765faa510d59f82e64c7322
Scholar <scholar@paradigm.local>
2023-09-08 11:29:41 -0600

    intro to concepts

--- ntro to Key Programming Concepts.py
+++ ntro to Key Programming Concepts.py
@@ -7,3 +7,80 @@ def give_a_name():
     print (my_name)
 
     user_input = input("what is your name? ")
+    print("Nice to meet you, {user_input}!")
+    #date types  
+
+    my_string = "This is my string."
+    my_interger = 1456
+    my_float = 3.14
+    my_float = 3.0
+
+    print{my_interger} + my_float
+
+    is_my_boolean = True 
+    print(my_float + my_float)
+
+    my_declaration = ""
+    my_initialization = "Hello world"
+
+    add = 1 + 2
+    subtract = 2 - 1
+    divide = 2 / 1
+    multiply = 1 * 3
+    modulo = 2 % 1 
+    exponent = + 2 ** 3
+
+    print {modulo} 
+
+    statement = 4 > 2
+    statement2 = 4>= 2
+    statement3 = 4 <= 2
+    statement4 = 4 == 2
+    statement5 = 4 != 2
+
+    messege = 4 > 2 and 33 > 43
+    messege = 4 < 2 or 33 < 43
+    X = True 
+ 
+
+    print(messege)
+print{messege2} 
+
+# PEMMDAS
+
+y = 10
+x = y >= 10
+
+z = 7 // 2
+t= 7 / 2 
+print(z) 
+
+print(t)
+
+
+def my_function():
+print(" I am so great")
+
+
+my_function()
+
+def add(num1, num2):
+print{num1 + num2}
+
+my_name = "tacocat"
+words = my_name.split("o")
+print(words)
+
+def subtract(num1, num2):
+    return num1 - num2 
+
+difference = subtract(4,2)
+
+print(difference)
+
+[1,2,3,4]
+{"name"}
+'bob'
+'job': 'janitor'
+'is_male': True 
+ 
