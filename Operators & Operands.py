9e6ed3ee89549ca018373c6e37b09c21e6754d67
Scholar <scholar@paradigm.local>
2023-09-12 13:36:28 -0600

    fixed

---
+++ Operators & Operands.py
@@ -0,0 +1,28 @@
+from numbers import Integral
+
+
+modulus = 10 % 3
+print(modulus)
+
+multiplication = 5 * 3
+print(multiplication) 
+
+integer_division = 7 // 2
+
+float_division = 7 / 2
+print(float_division) 
+
+addition = 1 + 2
+print(addition)
+
+subtraction = 78 - 45
+print(subtraction) 
+
+first = 40 > 10
+second = 10 < 20
+third = 10 == 10
+forth = 100 >= 70
+fith = 497 <= 593
+sixth = 50 = 50 
+
+
