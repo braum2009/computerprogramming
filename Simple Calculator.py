6ad628a0d390acc97e191bbc941753c08d802a95
Scholar <scholar@paradigm.local>
2023-09-26 13:28:19 -0600

    simple calculator

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,6 +1,6 @@
-first_number = input("What is the first number? ")
-second_number = input("What is the second number? ")
-operation =  input ("what operation would you like to do: add, subtract, multiply, or divide?")
+first_number = int(input("What is the first number? "))
+second_number = int(input("What is the second number? "))
+operation =  input ("what operation would you like to do: add, subtract, multiply, or divide? ")
 
 def add(num1, num2):
     print(f"{num1} + {num2} = {num1 + num2}")
@@ -14,4 +14,11 @@ def multiply(num1, num2):
 def divide(num1, num2):
     print(f" {num1} / {num2} = {num1 / num2} ")
 
-    
+if operation == 'add':
+    add(first_number, second_number)
+elif operation == 'subtract':
+    subtract(first_number, second_number)
+elif operation == 'multiply':
+    multiply(first_number, second_number)
+elif operation == 'divide':
+    divide(first_number, second_number)
