050596aee1e4b15f2d9fd86c31874a64058fd2de
Scholar <scholar@paradigm.local>
2023-10-31 13:36:22 -0600

    interview

---
+++ Project - Programming Career Interview.py
@@ -0,0 +1,28 @@
+interviewee = input("Hello thank you for comming can I get your name? ")
+
+print(f"Hello {interviewee}. Let's get started.")
+
+programming = input("what types of promgramming are you interested in? ")
+
+experience = input("How many years of experience do you have?")
+
+desired_salary = input("What is your desired salary?")
+
+enjoyment_or_money = input("Are you working here for the money or the enjoyment?")
+
+why = input("why did you choose this job?")
+
+did_you = input("did you take any risks with this job")
+
+retire_time = input("when do you plan on retiring?")
+
+different_job = input("would you choose this job for another?")
+
+regretfulness = input("do you regret choosing this job over being a tech guy at one of the biggest facilitys in the country?")
+
+position = input("what is your disired position?")
+
+
+
+
+
