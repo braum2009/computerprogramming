25425582a4ad12081eafdfd232cd6cd870127e09
Scholar <scholar@paradigm.local>
2023-12-05 13:34:34 -0700

    final

---
+++ Project - RPG.py
@@ -0,0 +1,44 @@
+print("Welcome to my own personal RPG")
+
+admin = input("what is your name? ")
+
+print(f"Nice to meet you {admin}!")
+
+# print statement that asks them to enter a w or i
+walk = input("to walk press w or i ")
+
+# print that states they are walking
+print ("Nice job your a starting to get the hang of it")
+
+# 3 different prints that says you ran into an enemy\
+print ("be careful there is an enemy near by watch out!")
+print ("there is a way of detecting enemys if you see mud tracks there is someone near by watch out!")
+print ("There is a item in the game that can be useful but used against you it can track player watch out somoen found you!")
+
+option = input("would you like to fight or run")
+
+
+# input that asks if you want to fight or run
+
+print(f" are you sure this enemy is a lvl 10 threat {admin}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
